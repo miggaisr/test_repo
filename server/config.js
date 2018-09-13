f6c8f995b269f5f79b73e0f718a6aed76ea8acd5
Gadi Israeli <gadi.israeli@flex.com>
2018-09-12 22:52:48 -0700

    [Extension] Update application settings

--- server/config.js
+++ server/config.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
     // Start URL that browsing should start at.
-    startURL: '10.229.8.35/qms3/login.php',
+    startURL: 'http://10.229.8.35/qms3/login.php',
     // Start Method when there is no start URL.  This is fpr when we are doing an API integration,
     // and we don't actually need to load phantom (yet).
     // startMethod: "login.onStart",
